3eff1be55cb41064dc4d2483f864bfe38c32966e
abelladius <90325715+abelladius@users.noreply.github.com>
2023-10-20 17:23:13 +0300

    KYD Animal Shelter Preview
    This is just a sneak peek! I'm working on the full version with more goodies.

---
+++ index.js
@@ -0,0 +1,55 @@
+    // Burger functionality
+const burger = document.querySelector('.burger');
+const navItems = document.querySelector('.nav-items');
+
+burger.addEventListener('click', () => {
+    navItems.classList.toggle('open');
+})
+
+// 
+
+    // Contact form
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.getElementById("contact-form");
+        const successMessage = document.getElementById("success-message");
+    
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+    
+            // Simulate form submission (you can replace this with an actual API call)
+            setTimeout(function() {
+                successMessage.textContent = "Message sent successfully!";
+                form.reset();
+            }, 1000);
+        });
+    });
+
+
+    // Gallery popup
+
+    function showImage(imageNumber) {
+        var modal = document.getElementById('image-modal');
+        var modalImg = document.getElementById('modal-content');
+        
+        modal.style.display = 'block';
+        modalImg.src = 'images/gallery-' + imageNumber + '.jpeg';
+    }
+    
+    function closeImage() {
+        var modal = document.getElementById('image-modal');
+        modal.style.display = 'none';
+    }
+
+    // function showImage(imageNumber) {
+    //     var modal = document.getElementById('image-modal');
+    //     var modalImg = document.getElementById('modal-content');
+        
+    //     modal.style.display = 'flex';
+    //     modalImg.src = 'images/gallery-' + imageNumber + '.jpeg';
+    // }
+    
+    // function closeImage() {
+    //     var modal = document.getElementById('image-modal');
+    //     modal.style.display = 'none';
+    // }
